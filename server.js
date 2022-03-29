360657bd4537228bbfab6c844ef2aa28d6fe04ed
Adrien GOGOIS <ad.gogois@gmail.com>
2022-03-29 15:19:32 +0200

    Create route to add new student with address ppulate

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+// Import modèles mongoose:
+const Student = require("./models/studentModel");
+const Address = require("./models/addressModel");
+
+app.use(express.json());
+
 mongoose
   .connect(
     "mongodb+srv://adrien:supermotdepasse@cluster0.sjoau.mongodb.net/morning-populate?retryWrites=true&w=majority",
@@ -15,6 +21,34 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Welcome");
+});
+
+app.post("/students", (req, res) => {
+  const address1 = new Address({
+    streetName: req.body.address.streetName,
+    streetNumber: req.body.address.streetNumber,
+    postCode: req.body.address.postCode,
+    city: req.body.address.city,
+  });
+
+  address1.save(function (err) {
+    const student1 = new Student({
+      firstName: req.body.firstName,
+      surname: req.body.surname,
+      address: address1._id,
+    });
+
+    student1.save((err) => {});
+
+    res.status(201).json({
+      message: "Etudiant ajouté",
+    });
+  });
+});
+
+app.get("/students/:id", async (req, res) => {
+  const student = await Student.findById(req.params.id).populate("address");
+  res.json(student);
 });
 
 app.listen(8000, () => {
